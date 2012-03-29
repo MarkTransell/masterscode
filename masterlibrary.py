3fe989160a294d7ecd70050c05f9eeac622d38c9
Mark Transell <marktransell@gmail.com>
2012-03-29 12:22:23 +0200

    Changed the TPA object for scoring matrices to give integer values in line with usual BLAST format, added function to convert back from symbols using naive PAA to data for illustrative purposes

--- masterlibrary.py
+++ masterlibrary.py
@@ -50,19 +50,23 @@ class TPAdistance():
 					self.lookuptable[q, z] = self.breakpoints[numpy.max([q,z])-1] - self.breakpoints[numpy.min([q,z])]
 				else:
 					self.lookuptable[q, z] = 0
+		
+		#Normalising the table to approximate the BLOSUM62 matrix original format
+		
+		self.lookuptable = 1 + numpy.round(-self.lookuptable/float(self.lookuptable[0, 2]))
 					
 	def save(self, filename='BLOSUM62'):
 		"""A method to rewrite the psiBLAST substitution matrix stored at /usr/share/ncbi/data/(filename)"""
 		scorefile = open('/usr/share/ncbi/data/' + filename, "w")
 		protstring = 'ARNDCQEGHILKMFPSTWYVBJZX*'
 		savematrix = numpy.zeros((25,25))
-		savematrix[0:len(self.lookuptable), 0:len(self.lookuptable)] = 0 - self.lookuptable[:,:]
+		savematrix[0:len(self.lookuptable), 0:len(self.lookuptable)] = self.lookuptable[:,:]
 		j = 0
-		scorefile.write('   A  R  N  D  C  Q  E  G  H  I  L  K  M  F  P  S  T  W  Y  V  B  J  Z  X  *\n')
+		scorefile.write("   " + " ".join(protstring) + "\n")
 		for char in protstring:
 			scorefile.write(char + '  ')
 			for i in range(25):
-				scorefile.write(str(numpy.round(savematrix[j, i], decimals=3)) + '  ')
+				scorefile.write(str(int(savematrix[j, i])) + '  ')
 			scorefile.write('\n')
 			j = j + 1
 		scorefile.close()
@@ -172,21 +176,6 @@ class Dataset():
 		
 			stringreturn = stringreturn + charac
 			numbertodo = numbertodo + nsampleseg
-				
-			
-			
-#			if avgval <= 0.25:
-#				charac = "E"
-#			elif avgval <= 0.43:
-#				charac = "D"
-#			elif avgval <= 0.57:
-#				charac = "T"
-#			elif avgval <= 0.75:
-#				charac = "M"
-#			else:
-#				charac = "C"
-#			stringreturn = stringreturn + charac
-#			numbertodo = numbertodo + nsampleseg
 			        	
 		return stringreturn
 
@@ -206,7 +195,7 @@ class Sequence():
 	def __init__(self, filename='default', format='fa', seq=None, identity='unknown',descrip='unknownsequence'):
 		self.seqs = []
         
-		if seq:
+		if seq is not None:
 			self.list = SeqRecord(MutableSeq(seq), id=identity, description=descrip)
 		else:
 			handle = open(filename + '.' + format)
@@ -227,29 +216,32 @@ class Sequence():
 		SeqIO.write(self.list, handle, "fasta")
 		handle.close()
 		
-	def simpledataconvert(self, resolution):
+	def simpledataconvert(self, resolution, TPAobject):
 		"""Convert the sequence data back to approximated dataset by naive PAA, (process info is lost, only for demonstration purposes"""
-		data = []
+		
+		protstring = 'ARNDCQEGHILKMFPSTWYVBJZX*'
+		data = [];
 		for i in range(len(self.seqs)):
-			if self.seqs[i] == "E":
-				val = 0.125
-			elif self.seqs[i] == "D":
-				val = 0.34
-			elif self.seqs[i] == "T":
-				val = 0.5
-			elif self.seqs[i] == "M":
-				val = 0.64
-			else:
-				val = 0.875
+			for j in range(len(protstring)):
+				if cmp(self.seqs[i], protstring[0]) == 0:
+						value = TPAobject.breakpoints[0] - 0.5*(TPAobject.breakpoints[1] - TPAobject.breakpoints[0])
+					
+				elif cmp(protstring[-1], self.seqs[i]):
+						value = TPAobject.breakpoints[-1] + 0.5*(TPAobject.breakpoints[-1] - TPAobject.breakpoints[-2])
+					 
+				elif cmp(protstring[i], self.seqs[i]) == 0:
+						value = 0.5*(TPAobjec.breakpoints[j] - TPAobject.breakpoints[j-1])
+					
 			for i in range(resolution*2):
-				data.append(val)
+				data.append(value)
 				
 		return data
-		
+
+
         
 class Match():
 	def __init__(self, filename='default', darray=None):
-		if darray:
+		if darray is not None:
 			self.matchrecs = darray
 		else:
 			handle = open(filename + '.csv')
@@ -278,22 +270,18 @@ class Match():
 
 x = Dataset('QueryDat')
 query = Dataset('QueryDat')
-Q = TPAdistance(8, x)
-Q.save('BLOSUMTEST')
-#query.boxfilter(2)
-q = Sequence(seq=query.simplestringconvert(2, Q), descrip='QUERYTEST', identity='Q1')
+Q = TPAdistance(6, x)
+Q.save('BLOSUM62')
+print Q.lookuptable
+q = Sequence(seq=query.simplestringconvert(6, Q), descrip='QUERYTEST', identity='Q1')
 q.save('querytest')
-#x.boxfilter(2)
-p = x.simplestringconvert(2, Q)
+p = x.simplestringconvert(6, Q)
 y = Sequence(seq=p, descrip='CONVERSIONTEST', identity='T1')
+print y.simpledataconvert(6, Q)
 y.save('dbtest')
 dustmask('dbtest')
 makedb('dbtest', 'dbtest')
 psiblastme('querytest', 'dbtestdb', 'fulltest')
-##handle = open('fulltest.csv')
-##recs = csv.reader(handle, delimiter = ',')
-##print [float(x) if '.' in x else int(x) if isempty(RWE(x)) for x in recs.next()]
-##print [try float(x) except x for x in recs.next()]
 blastmatch = Match(filename='fulltest')
 print blastmatch.evalue
 print blastmatch.Qstart
@@ -306,12 +294,5 @@ print blastmatch.Qstart
 #print Q.lookuptable
 z = Dataset('Data')
 p = Dataset('QueryDat')
-print len(z.recarray)
-print len(z.recarray[:,0])
-print len(z.recarray[0,:])
-print z.recarray
-z.mutateinsert(p)
-print z.recarray
-
 
 
